da4407f07144412aa143e0d10c9f727025865738
NaFFF345 <asjd581@gmail.com>
2017-09-18 23:05:18 +0900

    [add]Test page. [change]changing coding style mostly.

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-$(function (){
-	//window.alert("読み込みました");
-	$("head").html (
-	$("head").html().replace("https://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js","jquery-Google-1.10.0.js")
-	);
-	observer.observe(document, {
-    	attributes: true,
-    	childList: true,
-    	characterData: true,
-    	subtree: true
-	});
-});
+var jqread = document.getElementById('jqu');
+//alert("うんこもりもり");
+function rejq() {
+    //空div作成
+    var mdi = document.createElement("div");
+    //mdi.innerHTML = "";
+    //script加筆
+    var s1 = document.createElement("script");
+    s1.innerHTML = "<script src=\"jquery-Gppgle-1.10.0,js\"";
+    //子要素化設定
+    var bd = document.getElementsByTagName("body");
+    bd.appendChild(mdi);
+    mdi.appendChild(s1);
+
+    //var s = document.createElement("script");
+    //s.innerHTML = "alert(123)";
+    //var ele = document.getElementById("abc");
+    //ele.appendChild(s);
+    //jqread.innerHTML = "<script src='jquery-Google-1.10.0.js'>";
+}
+/*  observer.observe(document, {\
+     attributes: true,
+     childList: true,
+     characterData: true,
+     subtree: true
+ });*/
